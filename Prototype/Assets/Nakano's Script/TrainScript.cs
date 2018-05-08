12658a86c3a388670fb2229192fee91bcd3d671c
Hiroki Nkano <nkc.hiroki.naknao@gmail.com>
2018-05-08 14:17:06 +0900

    ランダムにステージ作成

---
+++ Prototype/Assets/Nakano's Script/TrainScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainScript : MonoBehaviour {
+
+    //配列
+    public GameObject[] Train;
+
+	// Use this for initialization
+	void Start () {
+        //ランダムな数を代入
+        int rdm = UnityEngine.Random.Range(0, 3);
+
+        //確認用
+       // Debug.Log(rdm);
+
+        //配列のオブジェクトからrdm番目のオブジェクトを表示
+        Instantiate(Train[rdm], transform.position, transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
