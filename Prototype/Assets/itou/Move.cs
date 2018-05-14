46586a3b2fef3c0020a3c79dce2057daac2cfc63
Kaito Itou <nkc.kaito.itou@gmail.com>
2018-05-14 14:47:04 +0900

    気持ち悪い挙動を緩和

--- Prototype/Assets/itou/Move.cs
+++ Prototype/Assets/itou/Move.cs
@@ -3,68 +3,78 @@ using System.Collections;
 
 public class Move : MonoBehaviour
 {
-    float speed = 0.05f;
-    float timer = 0.0f;
-    int count = 0;
+    float speed = 0.05f;    //このオブジェクトのスピード
+    float timer = 0.0f;     //衝突してからの時間を計る用のタイマー
+    int count = 0;          //何回壁に当たったかをカウントしておくカウンタ―
 
-    bool flag = false;
+    bool collisionFlag = false; //衝突検知フラグ
+
+    BoxCollider boxCollider;
+    Vector3 boxColliderSize = new Vector3(0.5f, 0.5f, 2.5f);
+    Vector3 defaultSize;
 
     void Start()
     {
-
+        boxCollider = GetComponent<BoxCollider>();  //自分についているBoxColliderを取得
+        defaultSize = boxCollider.size;             //デフォルトの大きさを保存
     }
 
     void Update()
     {
         //向いている方向に移動
-        transform.position += transform.TransformDirection(Vector3.forward) * speed;
+        if (collisionFlag == false)
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * speed;
+        }
 
         //壁に当たったら時間を計る
-        if (flag == true)
+        if (collisionFlag == true)
         {
             timer += Time.deltaTime;
         }
-
-        if (timer < 0.5 && count >= 4)
+        Debug.Log(count);
+        //行き止まりになったら消滅
+        if (count >= 4)
         {
             Destroy(gameObject);
         }
 
-        //壁に当たってから0.1秒たったら時間を計ることをやめて、タイマーをリセット
-        if (timer > 0.5)
+        //壁に当たってから0.05秒たったら時間を計ることをやめて、タイマーをリセット
+        if (timer > 0.05)
         {
-            flag = false;
-            timer = 0;
-            count = 0;
+            boxCollider.size = defaultSize; //センサーを元に戻す
+            collisionFlag = false;          //フラグをオフに
+            timer = 0;                      //タイマーをリセット
+            count = 0;                      //カウンターをリセット
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         //壁に当たったら向いている方向から90度回転(右を向く)し、タイマーを起動
-        if (other.gameObject.tag == "Wall" && flag == false)
+        if (other.gameObject.tag == "Wall" && collisionFlag == false)
         {
             transform.Rotate(new Vector3(0, 90, 0));
-            flag = true;
+            boxCollider.size = boxColliderSize; //センサーを大きく伸ばす
+            collisionFlag = true;
             count++;
-            Debug.Log("起動１");
+        }
+
+        if (other.gameObject.tag == "Stage")
+        {
+            GameObject gameObj = other.gameObject;
+            transform.parent = gameObj.transform; //侵入したステージの子オブジェクトに
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        //一度壁に当たってから0.01秒以内にまだ壁に当たっていた場合180回転（左を向く）
-        if (other.gameObject.tag == "Wall" && flag == true && timer > 0.01)
+        //一度壁に当たってからまだ壁に当たっていた場合180回転（左を向く）
+        if (other.gameObject.tag == "Wall" && collisionFlag == true)
         {
             transform.Rotate(new Vector3(0, 180, 0));
             count++;
-            Debug.Log("起動２");
         }
-        //if (other.gameObject.tag == "Wall" && flag == true && timer < 0.5)
-        //{
-        //    transform.Rotate(new Vector3(0, 180, 0));
-        //    Debug.Log("2_起動");
-        //}
     }
 }
 
