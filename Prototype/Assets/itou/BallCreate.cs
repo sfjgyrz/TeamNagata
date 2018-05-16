3a649e2430af03e7da2fa811fcbe3369dab5c573
Kaito Itou <nkc.kaito.itou@gmail.com>
2018-05-16 13:56:57 +0900

    ボールを生産するやつのスクリプトを作成

---
+++ Prototype/Assets/itou/BallCreate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCreate : MonoBehaviour {
+    public GameObject ball;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Instantiate(ball, transform.position, Quaternion.identity);
+            ball.transform.Rotate(new Vector3(0, transform.rotation.y, 0));
+            transform.Rotate(new Vector3(0, 90, 0));
+        }
+    }
+}
